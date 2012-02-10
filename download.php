bcae578e33c08bde4b50b89a34bab0b9083388dc
ShadowEO <dreamcaster23@gmail.com>
2012-02-10 12:21:59 -0500

    Fixed Downloading Videos through Mobile interface
    Downloaded files from Mobile Interface now download with a filename
    created by their series name-S<season>E<episode>.<originalextension>

--- download.php
+++ download.php
@@ -19,19 +19,49 @@
 	 */
 		
 	
+	
+	
 	/* TODO: Filename support and path substitution on Samba Shares */
 
 	/* PROPOSED: merge to stream.php */
 	
-	header("Content-Type: application/octet-stream");
+
 	require('libs/XBMCHelper.class.php');
 		$XBMC = new XBMCHelper();
 		$videoInformation = $XBMC->GetEpisodeInformation($_GET['e']);
-		$file = $videoInformation['filePath'];
 		ini_set("max_execution_time", "0");
+		$file = $videoInformation['filePath'];
+		$streampath = parse_url($file);
+		if(isset($streampath['scheme']) && $streampath['scheme'] == "smb://")
+		{
+			// We found that the filepath includes smb://, by this we have determined a 
+			// Synchronized XBMC installation and substitute direct paths for searching for the file
+			// In the library.
+			$di = new RecursiveDirectoryIterator($XBMC->XBMCTVLibraryPath);
+			foreach(new RecursiveIteratorIterator($di) as $filename=>$cur)
+			{
+				if($cur->getFilename() == $videoInformation['filename'])
+				{
+					$file = $filename;
+					break;
+				}
+			}
+		}
+
 		$length = filesize($file);
-		header("Content-Filename: test.mp4".$videoInformation['filename']);
+		$pathparts = pathinfo($file);
+		$exten = $pathparts['extension'];
+		header("Pragma: public");
+		header("Expires: 0");
+		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+		header('Last-Modified: '.gmdate ('D, d M Y H:i:s', filemtime ($file)).' GMT');
+		header('Cache-Control: private',false);
+		$newfilename = $videoInformation['SeriesName']."-S".$videoInformation['season']."E".$videoInformation['episode'].".".$exten;
+		header('Content-Disposition: attachment; filename="'.$newfilename.'"');
+		header("Content-Transfer-Encoding: binary");
+		header("Content-Type: application/octet-stream");		
 		header("Content-Length: $length");
+		header("Connection: close");
 		readfile($file);
 
 ?>
